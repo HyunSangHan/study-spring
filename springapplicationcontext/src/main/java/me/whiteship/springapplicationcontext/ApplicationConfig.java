17374b22a8772311b154e896cd57dd5132911876
HyunSangHan <gustkd3@gmail.com>
2019-07-31 09:29:33 +0900

    IoC Container 1-2

---
+++ springapplicationcontext/src/main/java/me/whiteship/springapplicationcontext/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package me.whiteship.springapplicationcontext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
+public class ApplicationConfig {
+
+}
+
+//@Configuration
+//public class ApplicationConfig {
+//
+//    @Bean
+//    public BookRepository bookRepository() {
+//        return new BookRepository();
+//    }
+//
+//    @Bean
+//    public BookService bookService() {
+//        return new BookService();
+//    }
+//}
