8be196693f821688b8630b2abc5482057b9d2027
HyunSangHan <gustkd3@gmail.com>
2019-08-04 10:53:58 +0900

    Doesn't work

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -1,19 +1,19 @@
 package me.whiteship.demospring51;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1+1}")
+    int value;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService.getClass().toString());
+        System.out.println("=============================");
+        System.out.println(value);
     }
 }
